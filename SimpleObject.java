aa343ff721525d8c7aa03677324defbc19194437
Aman Sinha <96279990+amansinha0907@users.noreply.github.com>
2022-04-22 16:38:14 +0530

    constructor

---
+++ SimpleObject.java
@@ -0,0 +1,27 @@
+package javaprogram;
+
+public class SimpleObject 
+{
+	SimpleObject()
+	{
+		System.out.println("calling without agrument constructer ");
+	}
+	SimpleObject(int a)
+	{
+		System.out.println("calling with one argument constructer");
+		System.out.println("argument="+a);
+	}
+	SimpleObject(int a,int b)
+	{
+		System.out.println("calling with two argument constructer");
+		System.out.println("sum of argument="+(a+b));
+	} 
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		SimpleObject s1=new SimpleObject(10);
+		SimpleObject s2=new SimpleObject(10,20); 
+    }
+
+}
